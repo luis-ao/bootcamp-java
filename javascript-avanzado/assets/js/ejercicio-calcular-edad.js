a54ce0b986d3968172dbb0a85ba76b0460bb3f65
Luis Roberto <ao.luis.roberto@gmail.com>
2022-01-11 16:58:53 -0600

    11-01-2022

---
+++ javascript-avanzado/assets/js/ejercicio-calcular-edad.js
@@ -0,0 +1,6 @@
+let añoactual = 2022
+
+function getInformation(){
+    let añousuario = parseInt(prompt("En que año naciste?"));
+    document.write("<h1>", "Tu edad es ", añoactual - añousuario, "</h1>")
+}
---
+++ javascript-avanzado/assets/js/objetos.js
@@ -0,0 +1,44 @@
+/* let miObjeto = {
+    // pares de clave valor
+    nombre: 'Luis'
+} */
+
+/* let nombre = 'Luis';
+let edad = 31;
+let direccion = '13 de Enero';
+let numero = 812930;
+
+console.log(nombre); */
+
+/* let persona = {
+    nombre: { nombre:'Luis', apellidoPaterno: 'Alarcon',
+    apellidoMaterno: 'Olmos'},
+    edad: 26,
+    direccion: {
+        calle: '13 de enero',
+        municipio: 'tecamac'},
+    numero: [1294031, 1212313]
+};
+
+console.log(persona.nombre.apellidoMaterno); */
+
+let maceta = {
+    medidas: {
+        ancho: '10 cm',
+        alto: '15 cm',
+        largo: '10 cm'
+    },
+    material: [
+        'concreto', 
+        'ceramica', 
+        'yeso', 
+        'barro'
+    ],
+    planta: [
+        'palo de brasil',
+        'phothos',
+        'Portulacaria afra'
+    ]
+};
+
+console.log(maceta.planta[1])
